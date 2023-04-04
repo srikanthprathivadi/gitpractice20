95d35327b272b378bc315d494292277d0bb38ae5
sri <anewacforme@gmail.com>
2023-04-04 01:50:20 +0000

    edited to feature-101

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello
 {
 Public static void main(String args[])
 {
-System.out.println(“Hello after editing author information in feature-101 remote branch”);
+System.out.println(“Hello after editing author info from feature-101”);
 }
 }
 
